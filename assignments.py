c1a58c8019b3fa138041306326fe7daeaf825464
hfosnaugh <hannah.l.fosnaugh@gmail.com>
2025-08-28 18:05:50 -0400

    trying to parse dates

--- assignments.py
+++ assignments.py
@@ -3,6 +3,20 @@ import pandas as pd
 
 HTML = "assignmentScraper/assignments_scraper/Assignments_ MATH 214 FA 2025.htm"
 
+#need to do custom parsing
+def dt_parse(df):
+    #string parsing stuff
+    for i in range(0, len(df), 1):
+        date_str = df.iloc[i, 1]
+        date_str = date_str.split()
+        print(date_str)
+        #df.iloc[i, 0] = df.iloc[i, 0] + " " + date_str[2] + " " + date_str[3]
+        #print(type(df.iloc[i, 0]))
+    #Nov 9 at 12am
+    
+    #df['due_date'] = pd.to_datetime(df['due_date'], errors='coerce')
+    #print(df)
+
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     assignments = pd.DataFrame(columns=['title', 'due_date'])
@@ -11,7 +25,9 @@ def get_content(html):
         title = row.find(class_='ig-title').get_text(strip=True)
         due_date = row.find('span',class_='screenreader-only').get_text(strip=True)
         assignments.loc[len(assignments)] = [title, due_date]
-    print(assignments)
+    dt_parse(assignments)
+    #print(assignments)
+    #assignments.to_csv('assignments.csv', index=False)
     
 
 def parse():
