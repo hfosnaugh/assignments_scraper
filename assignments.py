2ea4fbb0c8ada3f286add6b31b1e2a065805bf26
Hannah Fosnaugh <hannah.l.fosnaugh@gmail.com>
2025-09-05 10:38:36 -0400

    Final product (#2)
    * uhhh
    
    * trying to parse dates
    
    * Finally finishedgit add assignments.py ! No error handling but small enough data set I can handle that by hand since I need to anyway for some aspects.

--- assignments.py
+++ assignments.py
@@ -3,6 +3,28 @@ import pandas as pd
 
 HTML = "assignmentScraper/assignments_scraper/Assignments_ MATH 214 FA 2025.htm"
 
+#need to do custom parsing
+def dt_parse(df):
+    monthlist = {'January':'01', 'February':'02', 'March':'03', 'April':'04', 'May':'05', 'June':'06', 'July':'07', 'August':'08', 'September':'09', 'October':'10', 'November':'11', 'December':'12'}
+    #string parsing stuff
+    for i in range(0, len(df), 1):
+        date_str = df.iloc[i, 1]
+        date_str = date_str.split()
+        #print(date_str)
+        df.iloc[i, 0] = df.iloc[i, 0] + " due " + date_str[2] + " " + date_str[3]
+        for month in monthlist:
+            if date_str[0] in month:
+                date_str[0] = monthlist[month]
+        #reassigns date format
+        year = "2025"
+        if int(date_str[0]) < 7:
+            year = "2026"
+        df.iloc[i, 1] = date_str[0]+"/"+date_str[1]+"/"+year
+        #print(df.iloc[i, 0]+" | "+df.iloc[i, 1])
+    
+    #df['due_date'] = pd.to_datetime(df['due_date'], errors='coerce')
+    #print(df)
+
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     assignments = pd.DataFrame(columns=['title', 'due_date'])
@@ -11,7 +33,8 @@ def get_content(html):
         title = row.find(class_='ig-title').get_text(strip=True)
         due_date = row.find('span',class_='screenreader-only').get_text(strip=True)
         assignments.loc[len(assignments)] = [title, due_date]
-    print(assignments)
+    dt_parse(assignments)
+    assignments.to_csv('assignmentScraper/assignments.csv', index=False)
     
 
 def parse():
