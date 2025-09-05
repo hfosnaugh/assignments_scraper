759b3420d49c3571c6f9b137397a50ff7f194d3c
Hannah Fosnaugh <hannah.l.fosnaugh@gmail.com>
2025-09-05 10:41:20 -0400

    Nb2 (#3)
    * uhhh
    
    * trying to parse dates
    
    * Finally finishedgit add assignments.py ! No error handling but small enough data set I can handle that by hand since I need to anyway for some aspects.
    
    * was scraping the wrong date for the due date - fixing rn, WIP
    
    * worksgit add assignments.py !

--- assignments.py
+++ assignments.py
@@ -10,7 +10,7 @@ def dt_parse(df):
     for i in range(0, len(df), 1):
         date_str = df.iloc[i, 1]
         date_str = date_str.split()
-        #print(date_str)
+        print(date_str)
         df.iloc[i, 0] = df.iloc[i, 0] + " due " + date_str[2] + " " + date_str[3]
         for month in monthlist:
             if date_str[0] in month:
@@ -20,7 +20,7 @@ def dt_parse(df):
         if int(date_str[0]) < 7:
             year = "2026"
         df.iloc[i, 1] = date_str[0]+"/"+date_str[1]+"/"+year
-        #print(df.iloc[i, 0]+" | "+df.iloc[i, 1])
+        print(df.iloc[i, 0]+" | "+df.iloc[i, 1])
     
     #df['due_date'] = pd.to_datetime(df['due_date'], errors='coerce')
     #print(df)
@@ -28,11 +28,19 @@ def dt_parse(df):
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     assignments = pd.DataFrame(columns=['title', 'due_date'])
+    #title
     elements = soup.find_all(class_="ig-info")
     for row in elements:
         title = row.find(class_='ig-title').get_text(strip=True)
-        due_date = row.find('span',class_='screenreader-only').get_text(strip=True)
-        assignments.loc[len(assignments)] = [title, due_date]
+        assignments.loc[len(assignments)] = [title, ""]
+    #due date
+    elements2 = soup.find_all(class_="ig-details__item assignment-date-due")
+    duedates = []
+    for i in range(0, len(elements2), 1):
+        due_date = elements2[i].find('span',class_='screenreader-only').get_text(strip=True)
+        duedates.append(due_date)
+        assignments.iloc[i, 1] = duedates[i]
+    print(assignments)
     dt_parse(assignments)
     assignments.to_csv('assignmentScraper/assignments.csv', index=False)
     
@@ -43,3 +51,4 @@ def parse():
     get_content(html)
 
 parse()
+
