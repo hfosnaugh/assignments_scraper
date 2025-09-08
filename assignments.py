3c22175528b3a3e1e599aca30cac3e685cf5f045
hfosnaugh <hannah.l.fosnaugh@gmail.com>
2025-09-08 14:31:39 -0400

    iusgfiwugef

--- assignments.py
+++ assignments.py
@@ -42,7 +42,7 @@ def get_content(html):
         assignments.iloc[i, 1] = duedates[i]
     print(assignments)
     dt_parse(assignments)
-    assignments.to_csv('assignmentScraper/assignments.csv', index=False)
+    assignments.to_csv('assignmentScraper/mth214_assignments.csv', index=False)
     
 
 def parse():
